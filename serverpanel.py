83dae7a084953b577f15b2df063532282c7158f6
Sami <samiahmed0f0@gmail.com>
2021-10-24 17:47:21 +0600

    updated

--- serverpanel.py
+++ serverpanel.py
@@ -29,10 +29,8 @@ class tools:
       return True
 
 class operations(tools):
-   
-   def add_domain(self):
-      self.execute("clear")
-      domain_name = input("Enter domain name: ")
+
+   def create_domain_config(self,  domain_name):
       print("PLease wait adding domain..")
       self.execute(f"sudo mkdir -p {WEB_ROOT+domain_name}/public_html")
       print("working...")
@@ -71,6 +69,77 @@ class operations(tools):
       print("working...")
       self.execute(f"ls -lZ {WEB_ROOT+domain_name}/log")
       print("Successfully added domain")
+   
+   def add_domain(self):
+      self.execute("clear")
+      domain_name = input("Enter domain name: ")
+      self.create_domain_config(domain_name)
+      
+
+   def view_domains(self):
+      self.execute("clear")
+      domains_config = os.listdir(SITES_AVAILABLE)
+      print(" Domains:")
+      i = 1
+      for domain in domains_config:
+         split_res = domain.split(".")
+         if split_res[len(split_res) - 1] == "conf":
+            domain = domain.replace(".conf", "")
+            print(" "+str(i)+". "+domain)
+            i += 1
+
+   def get_domains(self):
+      domains_config = os.listdir(SITES_AVAILABLE)
+      domains = []
+      for domain in domains_config:
+         split_res = domain.split(".")
+         if split_res[len(split_res) - 1] == "conf":
+            domain = domain.replace(".conf", "")
+            domains.append(domain)
+
+      return domains
+   
+   def create_subdomain(self, subdomain_name, selected_domain):
+      subdomain_name = subdomain_name+"."+selected_domain
+      print("PLease wait adding subdomain..")
+      self.execute(f"sudo mkdir -p {WEB_ROOT+selected_domain}/{subdomain_name}/public_html")
+      print("working...")
+      self.execute(f"sudo mkdir -p {WEB_ROOT+selected_domain}/{subdomain_name}/log")
+      print("working...")
+      self.execute(f"sudo chown -R $USER:$USER {WEB_ROOT+selected_domain}/{subdomain_name}/public_html")
+      print("working...")
+      genrate_index_file = WEB_ROOT+selected_domain+"/"+subdomain_name+"/public_html/index.html"
+      self.write_file(genrate_index_file, "<h1>Successfully working</h1>")
+      print("working...")
+      self.execute(f"sudo chown -R $USER:$USER {WEB_ROOT+selected_domain}/{subdomain_name}/public_html")
+      print("working...")
+      genrate_domain_config = SITES_AVAILABLE+subdomain_name+".conf"
+      domain_config_content = f"""
+      <VirtualHost *:80>
+         ServerName {subdomain_name}
+         ServerAlias www.{subdomain_name}
+         DocumentRoot {WEB_ROOT+selected_domain}/{subdomain_name}/public_html
+         ErrorLog {WEB_ROOT+selected_domain}/{subdomain_name}/log/error.log
+         CustomLog {WEB_ROOT+selected_domain}/{subdomain_name}/log/requests.log combined
+      </VirtualHost>
+      """
+      self.write_file(genrate_domain_config, domain_config_content)
+      print("working...")
+      self.execute(f"sudo ln -s {SITES_AVAILABLE+subdomain_name}.conf {SITES_ENABLED+subdomain_name}.conf")
+      print("working...")
+      self.execute(f"sudo ls -dZ {WEB_ROOT+selected_domain}/{subdomain_name}/log/")
+      print("working...")
+      self.execute(f"sudo semanage fcontext -a -t httpd_log_t '{WEB_ROOT+selected_domain}/{subdomain_name}/log(/.*)?'")
+      print("working...")
+      self.execute(f"sudo restorecon -R -v {WEB_ROOT+selected_domain}/{subdomain_name}/log")
+      print("working...")
+      self.execute(f"sudo ls -dZ {WEB_ROOT+selected_domain}/{subdomain_name}/log/")
+      print("working...")
+      self.execute(f"sudo systemctl restart httpd")
+      print("working...")
+      self.execute(f"ls -lZ {WEB_ROOT+selected_domain}/{subdomain_name}/log")
+      print("Successfully added subdomain")
+            
 
 
 
@@ -81,8 +150,8 @@ class views(operations):
       !!--Domain Panel --!!
 
       1.Add Domain
-      3.Add Subdomain
-      2.View Domains
+      2.Create Subdomain
+      3.View Domains
 
       0 FOR BACK
       00 FOR GO HOME
@@ -96,6 +165,25 @@ class views(operations):
          self.window_main()
       elif COMMAND  == "1":
          self.add_domain()
+      elif COMMAND  == "2":
+         self.subdomain_creating()
+      elif COMMAND  == "3":
+         self.view_domains()
+
+   def subdomain_creating(self):
+      self.execute("clear")
+      print(" Please choose a domain: ")
+      domains = self.get_domains()
+      i = 1
+      for domain in domains:
+         print(" "+str(i)+". "+domain)
+         i += 1
+      get_domain = input(" Domain No>>> ")
+      get_domain = int(get_domain) - 1
+      selected_domain = domains[get_domain]
+      subdomain_name = input(" Subdomain name>> ")
+      self.create_subdomain(subdomain_name, selected_domain)
+         
 
 
    def window_main(self):
